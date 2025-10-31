f58d8f7a8332d0d7577fecd468fe4f1adc4f6f99
unknown <roy.utsab01@gmail.com>
2025-10-31 19:02:24 +0530

    first commit

---
+++ src/Address/BaseUrl.js
@@ -0,0 +1,11 @@
+// export const url='https://192.168.1.227:3011'
+
+// export const url='https://apinextgen.opentech4u.co.in:3011'
+// export const url = "http://192.168.1.238:3000"
+// export const url = "http://192.168.1.239:3000"
+// export const url = "http://202.21.38.178:3012"
+
+// export const url = "http://192.168.1.33:3014" //Local
+
+export const url = "https://ssvwsadmin.opentech4u.co.in" // Production
+
